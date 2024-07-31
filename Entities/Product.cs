b117bce4732e57655fe7672991f4a29f8efecf80
Igor Gabriel <souzaamorimdasilvai@gmail.com>
2024-07-30 22:39:00 -0300

    Created class Product

---
+++ Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Globalization;
+
+namespace EletronicsStore.Entities
+{
+    class Product
+    {
+        public string Name { get; protected set; }
+        public double Price { get; protected set; }
+
+        public Product() 
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Name} $ {Price.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
